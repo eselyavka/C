5d099b468050b0a2ef809e235c5f548e38b64f92
eselyavka <evg.selyavka@gmail.com>
2014-07-14 21:38:02 +0400

    add wordinverter source

---
+++ wordinverter/wordinverter.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void inverter(char *);
+void help(char *);
+
+int main(int argc, char *argv[]) {
+
+  if (*(argv + 1) == NULL ) {
+    help(argv[0]);
+    exit(0);
+  }
+
+  if ( (strncmp("-h", argv[1], strlen("-h")) == 0) || (strncmp("--help", argv[1], strlen("--help"))) == 0 ) {
+    help(argv[0]);
+    exit(0);
+  }
+
+  if( argc == 2 ) {
+    inverter( argv[1] ); 
+  } else {
+    printf("To many arguments\n");
+    help(argv[0]);
+  }
+
+  return 0;
+}
+
+void help(char *scriptName) {
+  printf("\tUsage: %s <string>\n\n\tExample: test->tset\n", scriptName);
+}
+
+void inverter(char *words) {
+  int i;
+  for (i=strlen(words)-1; i>=0; i--) {
+    printf("%c",words[i]);
+  }
+  printf("\n");
+}
